7ae9d0fc58fc62f3c0a205d2c62abc6b88cf3c1a
hao2005128@126.com <hao2005128@126.com>
2017-05-13 16:28:43 +0800

    修改

--- qiye_collection.sql
+++ qiye_collection.sql
@@ -31,5 +31,5 @@ CREATE TABLE `yuncai_company_evaluate` (
 -- ----------------------------
 -- Records of yuncai_company_evaluate
 -- ----------------------------
-INSERT INTO `yuncai_company_evaluate` VALUES ('4ci3w6toqcm6omvgvps9s967ftf45bav', '江苏筑牛网络科技有限公司苏州运营中心', 'http://127.0.0.1:83/caiji/html/jiangsu/9f191e3450b1c3aff0249d1ac8b234b7.html', '2016-09-12 15:18:02', '江苏省');
-INSERT INTO `yuncai_company_evaluate` VALUES ('c7dg7j83qfojuj7mkphwjv2wekct7e55', '江苏筑牛网络科技有限公司', 'http://127.0.0.1:83/caiji/html/jiangsu/49875773beff87ef949235655d012b64.html', '2016-09-12 15:18:15', '江苏省');
+INSERT INTO `yuncai_company_evaluate` VALUES ('4ci3w6toqcm6omvgvps9s967ftf45bav', '222', 'http://127.0.0.1:83/caiji/html/jiangsu/9f191e3450b1c3aff0249d1ac8b234b7.html', '2016-09-12 15:18:02', '江苏省');
+INSERT INTO `yuncai_company_evaluate` VALUES ('c7dg7j83qfojuj7mkphwjv2wekct7e55', '222', 'http://127.0.0.1:83/caiji/html/jiangsu/49875773beff87ef949235655d012b64.html', '2016-09-12 15:18:15', '江苏省');
